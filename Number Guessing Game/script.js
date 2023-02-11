f00864bd1d03f1b1fafc3c417c7dc9dbe2680045
Abduraxmonov Jasur <97042865+Jasurbek1999@users.noreply.github.com>
2023-02-11 22:07:19 +0900

    last commit

--- Number Guessing Game/script.js
+++ Number Guessing Game/script.js
@@ -7,7 +7,7 @@ let num_of_guesses = 0;
 let guessed_nums = [];
 
 function play() {
-    let user_guess = document.querySelector('#guess').ariaValueMax;
+    let user_guess = document.querySelector('#guess').value;
     if(user_guess < 1 || user_guess > 100) {
         alert("Please enter a number between 1 and 100.");
     }
@@ -28,6 +28,12 @@ function play() {
         msg2.textContent = "No. of guesses: " + num_of_guesses;
         msg3.textContent = "Guessed numbers are: " + 
         guessed_nums;
+    }
+    else if(user_guess == answer){
+        msg1.textContent = "Woow You Win!";
+        msg2.textContent = "The number was: " + answer;
+        msg3.textContent = "You guessed in it " + num_of_guesses + " ";
+        document.querySelector("my_btn").disable = true;
     }
 
 }
