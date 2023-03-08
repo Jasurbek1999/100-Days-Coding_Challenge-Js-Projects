ffe075dd5e950f79bdfac72bffaa14bdefb14422
Abduraxmonov Jasur <97042865+Jasurbek1999@users.noreply.github.com>
2023-03-08 22:07:00 +0900

    initial commit

---
+++ sessionStorage DEMO/js/app.js
@@ -0,0 +1,37 @@
+const MOON = '🌙';
+const SUN = '☀️';
+const DARK_MODE = 'dark';
+const LIGHT_MODE = 'light';
+const DEFAULT_MODE = DARK_MODE;
+
+const btn = document.querySelector('#theme-switcher')
+
+init();
+
+function init() {
+    let storedMode = sessionStorage.getItem('mode');
+    if (!storedMode) {
+        storedMode = DEFAULT_MODE;
+        sessionStorage.setItem('mode', DEFAULT_MODE);
+    }
+    setMode(storedMode)
+}
+
+function setMode(mode = DEFAULT_MODE) {
+    if (mode === DARK_MODE) {
+        btn.textContent = SUN;
+        document.body.classList.add(DARK_MODE);
+    } else if (mode === LIGHT_MODE) {
+        btn.textContent = MOON;
+        document.body.classList.remove(DARK_MODE);
+    }
+}
+
+btn.addEventListener('click', function () {
+    let mode = sessionStorage.getItem('mode');
+    if (mode) {
+        let newMode = mode == DARK_MODE ? LIGHT_MODE : DARK_MODE;
+        setMode(newMode)
+        sessionStorage.setItem('mode', newMode);
+    }
+})
