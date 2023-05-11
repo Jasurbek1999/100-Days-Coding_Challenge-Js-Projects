b4bf6379125b208a0af1e38bf4fb6a2dbe101ec6
Abduraxmonov Jasur <97042865+Jasurbek1999@users.noreply.github.com>
2023-05-11 21:50:22 +0900

    overall commit

---
+++ Live Character Counter/script.js
@@ -0,0 +1,5 @@
+let myText = document.getElementById('my-text');
+myText.addEventListener('input', () => {
+    let count = (myText.value).length;
+    document.getElementById('count-result').textContent = `Total characters: ${count}`;
+});
